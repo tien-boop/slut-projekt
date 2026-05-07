b179e53270f581031b014e21ce0920f21fe085bf
tien-boop <thuy.tien.tran@edu.huddinge.se>
2026-05-07 09:01:27 +0000

    lagt flask och testat lite med post grejen, nästa gågn ska lägga webbscraping thingy för att seracha grejer på wiki

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("search.html")
+
+@app.route("/", methods=["POST"])
+def show():
+    search = request.form["billionaire"]
+    return "Du gillar: " + search
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
