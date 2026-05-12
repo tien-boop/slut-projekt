ee9f9d25da3a9cb26ffcd572e4b002dbc1c955cc
tien-boop <thuy.tien.tran@edu.huddinge.se>
2026-05-12 11:09:44 +0000

    lagt beatifull soup

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from bs4 import BeautifulSoup
 
 from flask import Flask, render_template, request
 
+url = "https://en.wikipedia.org/wiki/The_World%27s_Billionaires"
+
+response = requests.get(url)
+
+html_content = response.text
+soup = BeautifulSoup(html_content, "html.parser")
+main_table = soup.select("table.wikitable tr td")
+
 app = Flask(__name__)
 
 @app.route("/")
