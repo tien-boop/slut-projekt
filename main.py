48df9c61b29d83b767718f1ae4b3ded6eddd2933
tien-boop <thuy.tien.tran@edu.huddinge.se>
2026-05-12 14:33:33 +0000

    ska fortsätta fixa året

--- main.py
+++ main.py
@@ -3,15 +3,27 @@ from bs4 import BeautifulSoup
 
 from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
 url = "https://en.wikipedia.org/wiki/The_World%27s_Billionaires"
 
 response = requests.get(url)
 
 html_content = response.text
 soup = BeautifulSoup(html_content, "html.parser")
-main_table = soup.select("table.wikitable tr td")
 
-app = Flask(__name__)
+# Hämta tabellen
+main_table = soup.select("table.wikitable")
+# Hämta individuella rutor i tabellen
+main_boxes = soup.select("table.wikitable tr td")
+# Hämta länkar i tabellen
+main_links = soup.select("table.wikitable tr td a")
+
+billionaires = []
+
+found = False
+
+
 
 @app.route("/")
 def home():
@@ -19,11 +31,29 @@ def home():
 
 @app.route("/", methods=["POST"])
 def show():
-    search = request.form["billionaire"]
-    return "Du gillar: " + search
 
+    if request.method == "POST":
 
+        year = request.form["billionaire"].strip()
 
+        # Hitta år
+        year_headers = soup.find("h3")
+
+        #!!!.vrf ska man ha for i detta fall vrf ska man ha innehållet av den!!for h3 in year_headers:
+
+            if h3.text == year:
+                
+                table = h3.find_next("table")
+
+            return str(table)
+
+        #if year_text:
+        #    hittar ordning på personer och namn
+
+        #else:
+        #    return "Året hittades inte"
+
+        return "Året hittades inte"
 
 if __name__ == "__main__":
     app.run(debug=True)
